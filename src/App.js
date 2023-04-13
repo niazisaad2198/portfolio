b7ae51d92f848b1c92b86ddf8cee9b89db32b582
Muhammad Saad Khan Niazi <niazisaad2198@gmail.com>
2023-04-14 00:36:20 +0500

    Changed file extension from jsx to js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="container box">
+        <h1>nav bar here</h1>
+      </div>
+      <div className="container content is-large">
+        <h1 className="title">Muhammad Saad Khan Niazi</h1>
+        <p className="subtitle">NodeJs Developer | 3D modeler | Video Editor</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
