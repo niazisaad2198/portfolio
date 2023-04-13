e5ccb6035f23ba54d216a8b73380af3933989542
Muhammad Saad Khan Niazi <niazisaad2198@gmail.com>
2023-04-14 01:06:52 +0500

    fix: file extension to jsx & add imports for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-function App() {
+import React from "react";
+import { Box, Container } from "react-bulma-components";
+
+const App = () => {
   return (
-    <>
-      <div className="container box">
+    <Container>
+      <Box className="container box">
         <h1>nav bar here</h1>
-      </div>
+      </Box>
       <div className="container content is-large">
         <h1 className="title">Muhammad Saad Khan Niazi</h1>
         <p className="subtitle">NodeJs Developer | 3D modeler | Video Editor</p>
       </div>
-    </>
+    </Container>
   );
-}
+};
 
 export default App;
